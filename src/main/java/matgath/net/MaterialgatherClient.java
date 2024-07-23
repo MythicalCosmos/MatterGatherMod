2b1ede1e428cf60cda7ffbd90c8f005e1871ae94
MythicalCosmos <360mcv@gmail.com>
2024-07-23 10:13:28 -0600

    1

---
+++ src/main/java/matgath/net/MaterialgatherClient.java
@@ -0,0 +1,9 @@
+package matgath.net;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaterialgatherClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/matgath/net/MaterialgatherDataGenerator.java
@@ -0,0 +1,11 @@
+package matgath.net;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MaterialgatherDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
